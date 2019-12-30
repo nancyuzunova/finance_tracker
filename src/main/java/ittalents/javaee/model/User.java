0a6514d6e9d974dec7ea6e7351d61f821358eb40
Lora Georgieva <lora.orli.georgieva@gmail.com>
2019-12-30 18:31:52 +0200

    add id annotation

--- src/main/java/ittalents/javaee/model/User.java
+++ src/main/java/ittalents/javaee/model/User.java
@@ -4,8 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
@@ -15,6 +14,8 @@ import java.time.LocalDateTime;
 @Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String firstName;
     private String lastName;
