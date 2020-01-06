86324db010523a12a8cf8b2f31f42fe1a58b3281
Lora Georgieva <lora.orli.georgieva@gmail.com>
2020-01-06 16:30:40 +0200

    create CurrencyConverter, add currency conversion in updateAccountCurrency method

---
+++ src/main/java/ittalents/javaee/model/CurrencyConverter.java
@@ -0,0 +1,21 @@
+package ittalents.javaee.model;
+
+import javax.money.Monetary;
+import javax.money.MonetaryAmount;
+import javax.money.convert.CurrencyConversion;
+import javax.money.convert.MonetaryConversions;
+
+public class CurrencyConverter {
+
+    public static double convert(Currency currencyFrom, Currency currencyTo, double amount) {
+        MonetaryAmount from = Monetary.getDefaultAmountFactory().setCurrency(currencyFrom.toString())
+                .setNumber(amount).create();
+        CurrencyConversion conversion = MonetaryConversions.getConversion(currencyTo.toString());
+        MonetaryAmount result = from.with(conversion);
+
+        double value = result.getNumber().doubleValueExact();
+        String formattedString = String.format("%.2f", value);
+
+        return Double.parseDouble(formattedString);
+    }
+}
