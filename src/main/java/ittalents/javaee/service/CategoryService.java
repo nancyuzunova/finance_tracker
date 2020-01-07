7b49c39b1832b98ac7cf1131ef487e52965b3e38
Lora Georgieva <lora.orli.georgieva@gmail.com>
2020-01-07 15:00:42 +0200

    create IconDao, implement changeCategoryIcon method

--- src/main/java/ittalents/javaee/service/CategoryService.java
+++ src/main/java/ittalents/javaee/service/CategoryService.java
@@ -1,12 +1,15 @@
 package ittalents.javaee.service;
 
 import ittalents.javaee.exceptions.ElementNotFoundException;
+import ittalents.javaee.exceptions.InvalidOperationException;
+import ittalents.javaee.model.dao.IconDao;
 import ittalents.javaee.model.pojo.Category;
 import ittalents.javaee.model.dto.CategoryDto;
 import ittalents.javaee.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -14,11 +17,13 @@ import java.util.Optional;
 @Service
 public class CategoryService {
 
-    private static CategoryRepository categoryRepository;
+    private CategoryRepository categoryRepository;
+    private IconDao iconDao;
 
     @Autowired
-    public CategoryService(CategoryRepository categoryRepository) {
+    public CategoryService(CategoryRepository categoryRepository, IconDao iconDao) {
         this.categoryRepository = categoryRepository;
+        this.iconDao = iconDao;
     }
 
     public List<CategoryDto> getAllCategories() {
@@ -39,15 +44,22 @@ public class CategoryService {
         throw new ElementNotFoundException("Category with id = " + id + " does not exist!");
     }
 
-    public List<String> getCategoryIcons(long id) {
-        Category category = getCategoryById(id);
-        List<String> categoryIcons = new ArrayList<>();
-        // TODO iconRepository.findIconsByCategoryId(id) ?
-        // category.getName().getIcons() ?
-        return categoryIcons;
+    public List<String> getCategoryIcons(long id) throws SQLException {
+        return iconDao.getIconsUrlsByCategoryId(id); // add SQLException to global handler
     }
 
     public void deleteCategory(long id) {
         this.categoryRepository.deleteById(id);
+    }
+
+    public void changeCategoryIcon(long categoryId, long iconId) throws SQLException {
+        Category category = getCategoryById(categoryId);
+        List<String> urls = getCategoryIcons(categoryId);
+
+        if (iconId < 0 || iconId >= urls.size()) {
+            throw new InvalidOperationException("There is not icon with id = " + iconId);
+        }
+
+        category.setIconURL(urls.get((int) categoryId));
     }
 }
