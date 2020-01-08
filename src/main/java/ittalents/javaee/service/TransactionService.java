b7eac72d68b0370ce96442e87eacf5f9c7e81165
Nancy Uzunova <nansy_98@abv.bg>
2020-01-08 16:02:52 +0200

    general edits

--- src/main/java/ittalents/javaee/service/TransactionService.java
+++ src/main/java/ittalents/javaee/service/TransactionService.java
@@ -3,11 +3,14 @@ package ittalents.javaee.service;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
 import ittalents.javaee.exceptions.ElementNotFoundException;
+import ittalents.javaee.exceptions.InvalidOperationException;
 import ittalents.javaee.model.dto.ResponseTransactionDto;
 import ittalents.javaee.model.dto.ResponseTransferDto;
+import ittalents.javaee.model.pojo.Account;
 import ittalents.javaee.model.pojo.Category;
 import ittalents.javaee.model.pojo.Transaction;
 import ittalents.javaee.model.dto.RequestTransactionDto;
+import ittalents.javaee.repository.AccountRepository;
 import ittalents.javaee.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,18 +27,24 @@ public class TransactionService {
 
     private TransactionRepository transactionRepository;
     private CategoryService categoryService;
+    private AccountRepository accountRepository;
 
     @Autowired
-    public TransactionService(TransactionRepository transactionRepository, CategoryService categoryService) {
+    public TransactionService(TransactionRepository transactionRepository, CategoryService categoryService, AccountRepository accountRepository) {
         this.transactionRepository = transactionRepository;
         this.categoryService = categoryService;
+        this.accountRepository = accountRepository;
     }
 
     public long createTransaction(long accountId, RequestTransactionDto requestTransactionDto) {
         Transaction transaction = new Transaction();
         transaction.fromDto(requestTransactionDto);
         transaction.setDate(LocalDateTime.now());
-        transaction.setAccount(accountId);
+        Optional<Account> acc = accountRepository.findById(accountId);
+        if(!acc.isPresent()){
+            throw new ElementNotFoundException("Account with id " + accountId + " does NOT exists");
+        }
+        transaction.setAccount(acc.get());
         transaction.setCurrency(requestTransactionDto.getCurrency());
         return this.transactionRepository.save(transaction).getId();
     }
@@ -67,7 +76,7 @@ public class TransactionService {
             document.add(new Chunk(
                     "Type: " + transaction.getType() + System.lineSeparator(),
                     contentFont)); // income or expense
-            Category category = categoryService.getCategoryById(transaction.getCategoryId());
+            Category category = categoryService.getCategoryById(transaction.getCategory().getId());
             document.add(new Chunk(
                     "Category: " + category.getName() + System.lineSeparator(),
                     contentFont));
